a4033021e3210c5b299588fc6abef31dc92bba0a
Daniel Sheppard <dans@dansheps.com>
2018-08-25 15:59:28 -0500

    Added Mount Data for special mounts

---
+++ BeStride_Constants.lua
@@ -0,0 +1,128 @@
+version = "0.5.0"
+
+mountData = {
+	[25953] = {
+		["name"] = "Blue Qiraji Battle Tank",
+		["type"] = "zone",
+		["zone"] = "taq",
+	},
+	[26054] = {
+		["name"] = "Red Qiraji Battle Tank",
+		["type"] = "zone",
+		["zone"] = "taq",
+	},
+	[26055] = {
+		["name"] = "Yellow Qiraji Battle Tank",
+		["type"] = "zone",
+		["zone"] = "taq",
+	},
+	[26056] = {
+		["name"] = "Green Qiraji Battle Tank",
+		["type"] = "zone",
+		["zone"] = "taq",
+	},
+	[5784] = {
+		["name"] = "Felsteed",
+		["type"] = "class",
+		["class"] = "warlock",
+	},
+	[23161] = {
+		["name"] = "Dreadsteed",
+		["type"] = "class",
+		["class"] = "warlock",
+	},
+	[23214] = {
+		["name"] = "Charger",
+		["type"] = "class",
+		["class"] = "paladin",
+	},
+	[13819] = {
+		["name"] = "Warhorse",
+		["type"] = "class",
+		["class"] = "paladin",
+	},
+	[55531] = {
+		["name"] = "Mechano-Hog",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[60424] = {
+		["name"] = "Mekgineer's Chopper",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[61425] = {
+		["name"] = "Traveler's Tundra Mammoth",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[61448] = {
+		["name"] = "Traveler's Tundra Mammoth",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[61467] = {
+		["name"] = "Grand Black War Mammoth",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[61465] = {
+		["name"] = "Grand Black War Mammoth",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[61470] = {
+		["name"] = "Grand Ice Mammoth",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[61469] = {
+		["name"] = "Grand Ice Mammoth",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[122708] = {
+		["name"] = "Grand Expedition Yak",
+		["type"] = "passenger",
+		["subtype"] = "ground",
+	},
+	[75973] = {
+		["name"] = "X-53 Touring Rocket",
+		["type"] = "passenger",
+		["subtype"] = "",
+	},
+	[93326] = {
+		["name"] = "Sandstone Drake",
+		["type"] = "passenger",
+		["subtype"] = "",
+	},
+	[121820] = {
+		["name"] = "Obsidian Nightwing",
+		["type"] = "passenger",
+		["subtype"] = "",
+	},
+	[245725] = {
+		["name"] = "Orgrimmar Interceptor",
+		["type"] = "passenger",
+		["subtype"] = "",
+	},
+	[245723] = {
+		["name"] = "Stormwind Skychaser",
+		["type"] = "passenger",
+		["subtype"] = "",
+	},
+}
+
+mountTypes = {
+	[230] = "ground",
+	[231] = "swimming",
+	[232] = "zone",
+	[241] = "zone",
+	[242] = "dead",
+	[247] = "flying",
+	[248] = "flying",
+	[254] = "swimming",
+	[269] = "ground",
+	[270] = "flying",
+	[284] = "chauffeured",
+}
