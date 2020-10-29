560a842bef99d5280f5a1e7997ba8412431a1ddb
Daniel Sheppard <dans@dansheps.com>
2020-10-29 09:16:59 -0500

    Update en-US.lua

--- localization/en-US.lua
+++ localization/en-US.lua
@@ -26,7 +26,7 @@ BeStride_Locale = {
 			Special = "Special"
 		}
 	},
-	About = "Version: " .. version .. "\n" .. "Author: " .. author .. "\n" .. "Discord: " .. discord .. "\n" .. "Description: " .. "\n\n" .. "        " .. "BeStride originally started out as YayMounts by Cyrae on Windrunner US and Anzu on Kirin Tor US" .. "\n" .. "        " .. "Later, Anaximander from Burning Legion US found the project was neglected and had several bugs which needed to be resolved" .. "\n" .. "        " .. "as part of the bug resolution process, the addon was modernized to make the code cleaner to follow as well as more modular." .. "\n\n" .. "Special Thanks:" .. "\n\n" .. "        " .. "Mindlessgalaxy: For helping with the beta testing",
+	About = "Version: " .. version .. "\n" .. "Author: " .. author .. "\n" .. "Discord: https://discord.gg/6bZf5PF" .. "\n" .. "Description: " .. "\n\n" .. "        " .. "BeStride originally started out as YayMounts by Cyrae on Windrunner US and Anzu on Kirin Tor US" .. "\n" .. "        " .. "Later, Anaximander from Burning Legion US found the project was neglected and had several bugs which needed to be resolved" .. "\n" .. "        " .. "as part of the bug resolution process, the addon was modernized to make the code cleaner to follow as well as more modular." .. "\n\n" .. "Special Thanks:" .. "\n\n" .. "        " .. "Mindlessgalaxy: For helping with the beta testing",
 	Bindings = {
 		Header = "BeStride",
 		Regular = "Mount Button",
