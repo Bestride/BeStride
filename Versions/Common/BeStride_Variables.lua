54c8545af900856f9c35f639b50b83bef92a01ac
Daniel Sheppard <dans@dansheps.com>
2022-11-17 00:13:31 -0600

    Change localization methodology

--- Versions/Common/BeStride_Variables.lua
+++ Versions/Common/BeStride_Variables.lua
@@ -1,68 +1,68 @@
 BeStride_Variables = {
 	Settings = {
 		Mount = {
-			{name="mount.enablenew",element="CheckBox",label=BeStride_Locale.Settings.EnableNew},
-			{name="mount.remount",element="CheckBox",label=BeStride_Locale.Settings.RemountAfterDismount},
-			{name="mount.emptyrandom",element="CheckBox",label=BeStride_Locale.Settings.EmptyRandom},
-			{name="mount.nodismountwhileflying",element="CheckBox",label=BeStride_Locale.Settings.NoDismountWhileFlying},
-			{name="mount.useflyingmount",element="CheckBox",label=BeStride_Locale.Settings.UseFlyingMount, dependants = {"mount.forceflyingmount"}},
-			{name="mount.forceflyingmount",element="CheckBox",label=BeStride_Locale.Settings.ForceFlyingMount, depends = {"mount.useflyingmount"}},
-			{name="mount.prioritizepassenger",element="CheckBox",label=BeStride_Locale.Settings.PrioritizePassenger},
-			{name="mount.noswimming",element="CheckBox",label=BeStride_Locale.Settings.NoSwimming},
-			{name="mount.flyingbroom",element="CheckBox",label=BeStride_Locale.Settings.FlyingBroom},
-			{name="mount.telaari",element="CheckBox",label=BeStride_Locale.Settings.Telaari},
-			{name="mount.forcerobot",element="CheckBox",label=BeStride_Locale.Settings.ForceRobot},
+			{name="mount.enablenew",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.EnableNew"]},
+			{name="mount.remount",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.RemountAfterDismount"]},
+			{name="mount.emptyrandom",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.EmptyRandom"]},
+			{name="mount.nodismountwhileflying",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.NoDismountWhileFlying"]},
+			{name="mount.useflyingmount",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.UseFlyingMount"], dependants = {"mount.forceflyingmount"}},
+			{name="mount.forceflyingmount",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.ForceFlyingMount"], depends = {"mount.useflyingmount"}},
+			{name="mount.prioritizepassenger",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.PrioritizePassenger"]},
+			{name="mount.noswimming",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.NoSwimming"]},
+			{name="mount.flyingbroom",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.FlyingBroom"]},
+			{name="mount.telaari",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Telaari"]},
+			{name="mount.forcerobot",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.ForceRobot"]},
 			{
 				element="Group",
 				children={
-					{name="mount.repair.use",element="CheckBox",label=BeStride_Locale.Settings.Repair.Use, dependants = {"mount.repair.force","mount.repair.durability","mount.repair.globaldurability","mount.repair.inventorydurability"} },
-					{name="mount.repair.force",element="CheckBox",label=BeStride_Locale.Settings.Repair.Force, depends = {"mount.repair.use"}},
-					{name="mount.repair.durability",element="Slider",label=BeStride_Locale.Settings.Repair.Durability,minDurability=0,maxDurability=100, increment=5, depends = {"mount.repair.use"}},
-					{name="mount.repair.globaldurability",element="Slider",label=BeStride_Locale.Settings.Repair.GlobalDurability,minDurability=0,maxDurability=100, increment=5, depends = {"mount.repair.use"}},
-					{name="mount.repair.inventorydurability",element="Slider",label=BeStride_Locale.Settings.Repair.InventoryDurability,minDurability=0,maxDurability=100, increment=5, depends = {"mount.repair.use"}},
+					{name="mount.repair.use",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Repair.Use"], dependants = {"mount.repair.force","mount.repair.durability","mount.repair.globaldurability","mount.repair.inventorydurability"} },
+					{name="mount.repair.force",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Repair.Force"], depends = {"mount.repair.use"}},
+					{name="mount.repair.durability",element="Slider",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Repair.Durability"],minDurability=0,maxDurability=100, increment=5, depends = {"mount.repair.use"}},
+					{name="mount.repair.globaldurability",element="Slider",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Repair.GlobalDurability"],minDurability=0,maxDurability=100, increment=5, depends = {"mount.repair.use"}},
+					{name="mount.repair.inventorydurability",element="Slider",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Repair.InventoryDurability"],minDurability=0,maxDurability=100, increment=5, depends = {"mount.repair.use"}},
 				}
 			}
 		},
 		Classes = {
 			{
-				{name="classes.deathknight.wraithwalk",element="CheckBox",label=BeStride_Locale.Settings.Classes.DeathKnight.WraithWalk,class="deathknight"},
+				{name="classes.deathknight.wraithwalk",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.DeathKnight.WraithWalk"],class="deathknight"},
 			},
 			{
-				{name="classes.demonhunter.felrush",element="CheckBox",label=BeStride_Locale.Settings.Classes.DemonHunter.FelRush,class="demonhunter"},
-				{name="classes.demonhunter.glide",element="CheckBox",label=BeStride_Locale.Settings.Classes.DemonHunter.Glide,class="demonhunter"},
+				{name="classes.demonhunter.felrush",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.DemonHunter.FelRush"],class="demonhunter"},
+				{name="classes.demonhunter.glide",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.DemonHunter.Glide"],class="demonhunter"},
 			},
 			{
-				{name="classes.druid.flightform",element="CheckBox",label=BeStride_Locale.Settings.Classes.Druid.FlightForm,class="druid", dependants = {"classes.druid.traveltotravel","classes.druid.flightformpriority","classes.druid.mountedtoflightform"}},
-				{name="classes.druid.traveltotravel",element="CheckBox",label=BeStride_Locale.Settings.Classes.Druid.TravelToTravel,class="druid", depends = {"classes.druid.flightform"}},
-				{name="classes.druid.flightformpriority",element="CheckBox",label=BeStride_Locale.Settings.Classes.Druid.FlightFormPriority,class="druid", depends = {"classes.druid.flightform"}},
-				{name="classes.druid.mountedtoflightform",element="CheckBox",label=BeStride_Locale.Settings.Classes.Druid.MountedToFlightForm,class="druid", depends = {"classes.druid.flightform"}},
+				{name="classes.druid.flightform",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Druid.FlightForm"],class="druid", dependants = {"classes.druid.traveltotravel","classes.druid.flightformpriority","classes.druid.mountedtoflightform"}},
+				{name="classes.druid.traveltotravel",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Druid.TravelToTravel"],class="druid", depends = {"classes.druid.flightform"}},
+				{name="classes.druid.flightformpriority",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Druid.FlightFormPriority"],class="druid", depends = {"classes.druid.flightform"}},
+				{name="classes.druid.mountedtoflightform",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Druid.MountedToFlightForm"],class="druid", depends = {"classes.druid.flightform"}},
 			},
 			{
-				{name="classes.hunter.aspectofthecheetah",element="CheckBox",label=BeStride_Locale.Settings.Classes.Hunter.AspectOfTheCheetah,class="hunter"},
+				{name="classes.hunter.aspectofthecheetah",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Hunter.AspectOfTheCheetah"],class="hunter"},
 			},
 			{
-				{name="classes.mage.slowfall",element="CheckBox",label=BeStride_Locale.Settings.Classes.Mage.SlowFall,class="mage"},
-				{name="classes.mage.blink",element="CheckBox",label=BeStride_Locale.Settings.Classes.Mage.Blink,class="mage"},
-				{name="classes.mage.blinkpriority",element="CheckBox",label=BeStride_Locale.Settings.Classes.Mage.BlinkPriority,class="mage"},
+				{name="classes.mage.slowfall",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Mage.SlowFall"],class="mage"},
+				{name="classes.mage.blink",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Mage.Blink"],class="mage"},
+				{name="classes.mage.blinkpriority",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Mage.BlinkPriority"],class="mage"},
 			},
 			{
-				{name="classes.monk.roll",element="CheckBox",label=BeStride_Locale.Settings.Classes.Monk.Roll,class="monk"},
-				{name="classes.monk.zenflight",element="CheckBox",label=BeStride_Locale.Settings.Classes.Monk.ZenFlight,class="monk"},
+				{name="classes.monk.roll",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Monk.Roll"],class="monk"},
+				{name="classes.monk.zenflight",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Monk.ZenFlight"],class="monk"},
 			},
 			{
-				{name="classes.paladin.steed",element="CheckBox",label=BeStride_Locale.Settings.Classes.Paladin.DivineSteed,class="paladin"},
+				{name="classes.paladin.steed",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Paladin.DivineSteed"],class="paladin"},
 			},
 			{
-				{name="classes.priest.levitate",element="CheckBox",label=BeStride_Locale.Settings.Classes.Priest.Levitate,class="priest"},
+				{name="classes.priest.levitate",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Priest.Levitate"],class="priest"},
 			},
 			{
-				{name="classes.rogue.sprint",element="CheckBox",label=BeStride_Locale.Settings.Classes.Rogue.Sprint,class="rogue"},
+				{name="classes.rogue.sprint",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Rogue.Sprint"],class="rogue"},
 			},
 			{
-				{name="classes.shaman.ghostwolf",element="CheckBox",label=BeStride_Locale.Settings.Classes.Shaman.GhostWolf,class="shaman"},
+				{name="classes.shaman.ghostwolf",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Shaman.GhostWolf"],class="shaman"},
 			},
 			{
-				{name="classes.evoker.hover",element="CheckBox",label=BeStride_Locale.Settings.Classes.Evoker.Hover,class="evoker"},
+				{name="classes.evoker.hover",element="CheckBox",label=LibStub("AceLocale-3.0"):GetLocale("BeStride")["Settings.Classes.Evoker.Hover"],class="evoker"},
 			},
 		}
 	},
@@ -163,8 +163,8 @@ defaults = {
 	}
 }
 
-_G["BINDING_HEADER_BESTRIDE_TITLE"] = BeStride_Locale.Bindings.Header
-_G["BINDING_NAME_CLICK BeStride_ABRegularMount:LeftButton"] = BeStride_Locale.Bindings.Regular
-_G["BINDING_NAME_CLICK BeStride_ABGroundMount:LeftButton"] = BeStride_Locale.Bindings.Ground
-_G["BINDING_NAME_CLICK BeStride_ABRepairMount:LeftButton"] = BeStride_Locale.Bindings.Repair
-_G["BINDING_NAME_CLICK BeStride_ABPassengerMount:LeftButton"] = BeStride_Locale.Bindings.Passenger
+_G["BINDING_HEADER_BESTRIDE_TITLE"] = LibStub("AceLocale-3.0"):GetLocale("BeStride")["Bindings.Header"]
+_G["BINDING_NAME_CLICK BeStride_ABRegularMount:LeftButton"] = LibStub("AceLocale-3.0"):GetLocale("BeStride")["Bindings.Regular"]
+_G["BINDING_NAME_CLICK BeStride_ABGroundMount:LeftButton"] = LibStub("AceLocale-3.0"):GetLocale("BeStride")["Bindings.Ground"]
+_G["BINDING_NAME_CLICK BeStride_ABRepairMount:LeftButton"] = LibStub("AceLocale-3.0"):GetLocale("BeStride")["Bindings.Repair"]
+_G["BINDING_NAME_CLICK BeStride_ABPassengerMount:LeftButton"] = LibStub("AceLocale-3.0"):GetLocale("BeStride")["Bindings.Passenger"]
--- localization/zh-TW.lua
+++ localization/zh-TW.lua
@@ -1,194 +1,105 @@
-if GetLocale() == "zhTW" then
-	BeStride_Locale = {
-		Options = {
-			OpenGUI = "開啟設定"
-		},
-		GUI = {
-			BUTTON = {
-				SelectAll = "全部選擇",
-				ClearAll = "清除全選",
-				Filter = "過濾器",
-				Copy = "複製",
-				Delete = "刪除"
-			},
-			TAB = {
-				Mounts = "坐騎",
-				MountOptions = "坐騎選項",
-				ClassOptions = "職業選項",
-				Keybinds = "快捷鍵",
-				Profile = "設定檔",
-				About = "關於",
-				Ground = "地面坐騎",
-				Flying = "飛行座騎",
-				Repair = "維修坐騎",
-				Swimming = "游泳坐騎",
-				Passenger = "多載坐騎",
-				Special = "特殊坐騎"
-			}
-		},
-		About = "版本: " .. version .. "\n" .. "作者: " .. author .. "\n" .. "描述: " .. "\n\n" .. "        " .. "BeStride 是由 YayMounts 改版而來原始作者為 Windrunner US 的 Cyrae 和 Kirin Tor US 的 Anzu" .. "\n" .. "        " .. "接下來, Burning Legion US 的 Anaximander 發現這個專案被原始作者放棄了, 並且有一些錯誤需要被修正" .. "\n" .. "        " .. "在解決錯誤的過程中, 此插件被現代化以讓程式碼更加的精簡與模組化." .. "\n\n" .. "特別感謝:" .. "\n\n" .. "        " .. "Mindlessgalaxy: 幫助 beta 版本的測試",
-		Bindings = {
-			Header = "BeStride",
-			Regular = "騎乘按鈕",
-			Ground = "強制地面坐騎按鈕",
-			Repair = "強制維修坐騎按鈕",
-			Passenger = "強制多載坐騎按鈕"
-		},
-		Zone = {
-			AzuremystIsle = {
-				Name = "Azuremyst Isle"
-			},
-			BloodmystIsle = {
-				Name = "Bloodmyst Isle"
-			},
-			Deadmines = {
-				Name = "死亡礦坑"
-			},
-			AQ = {
-				Name = "安其拉"
-			},
-			Dalaran = {
-				Name = "達拉然",
-				SubZone = {
-					Underbelly = {
-						Name = "城底區"
-					},
-					UnderbellyDescent = {
-						Name = "城底陷窟"
-					},
-					CircleofWills = {
-						Name = "意志之環"
-					},
-					BlackMarket = {
-						Name = "黑市"
-					},
-					KrasusLanding = {
-						Name = ""
-					}
-				}
-			},
-			Icecrown = {
-				Name = "寒冰皇冠"
-			},
-			Oculus = {
-				Name = "奧核之眼"
-			},
-			StormPeaks = {
-				Name = "風暴群山"
-			},
-			SholazarBasin = {
-				Name = "休拉薩盆地"
-			},
-			Wintergrasp = {
-				Name = "冬握湖"
-			},
-			Vashjir = {
-				Name = "瓦許伊爾",
-				SubZone = {
-					KelptharForest = {
-						Name = "凱波薩爾森林"
-					},
-					ShimmeringExpanse = {
-						Name = "閃光瀚洋"
-					},
-					AbyssalDepths = {
-						Name = "地獄深淵"
-					},
-					DamplightChamber = {
-						Name = "霧光之間"
-					},
-					Nespirah = {
-						Name = "奈斯畢拉"
-					},
-					LGhorek = {
-						Name = "勒苟雷克"
-					}
-				}
-			},
-			VortexPinnacle = {
-				Name = "漩渦尖塔"
-			},
-			Nagrand = {
-				Name = "納葛蘭"
-			}
-		},
-		Continent = {
-			Draenor = {
-				Name = "德拉諾"
-			}
-		},
-		Skills = {
-			Riding = {
-				Name = ""
-			}
-		},
-		Settings = {
-			EnableNew = "當學會新坐騎時自動啟用它們",
-			EmptyRandom = "如果沒有可以使用的已選擇坐騎時隨機使用",
-			RemountAfterDismount = "下馬後立刻重新上馬",
-			NoDismountWhileFlying = "在飛行時不解除坐騎. 你必須要降落或者是施放一個法術 (假使在選項中啟用後)",
-			UseFlyingMount = "在不可飛行的區域仍然使用飛行座騎",
-			ForceFlyingMount = "在不可飛行的區域強制使用飛行座騎",
-			PrioritizePassenger = "在隊伍時優先使用多載坐騎",
-			NoSwimming = "不会使用游泳坐骑",
-			FlyingBroom = "總是使用魔法掃帚代替普通坐騎",
-			Telaari = "在納葛蘭時總是使用泰拉蕊塔巴克或是霜狼",
-			ForceRobot = "當你有學草藥學時總是使用機器人",
-			Repair = {
-				Use = "當到達耐久度閥值時且擁有時使用維修坐騎",
-				Force = "當角色有維修坐騎時強制使用",
-				Durability = "物品低耐久度 %",
-				GlobalDurability = "全局低耐久度 %",
-				InventoryDurability = "背包物品低耐久度 %"
-			},
-			Classes = {
-				DeathKnight = {
-					WraithWalk = "死亡騎士: 闇境靈行"
-				},
-				Druid = {
-					FlightForm = "德魯伊: 使用飛行型態",
-					TravelToTravel = "德魯伊: 在戰鬥時, 從旅行或水棲型態直接切換回那個型態",
-					FlightFormPriority = "德魯伊: 即使未移動時也優先使用飛行型態",
-					MountedToFlightForm = "德魯伊: 當在飛行座騎上且正在飛行時,變換為飛行型態"
-				},
-				DemonHunter = {
-					FelRush = "惡魔獵人: 使用魔化衝刺",
-					Glide = "惡魔獵人: 使用滑翔"
-				},
-				Hunter = {
-					AspectOfTheCheetah = "Aspect of the Cheetah"
-				},
-				Mage = {
-					SlowFall = "法師: 緩落",
-					Blink = "法師: 閃現",
-					BlinkPriority = "法師: 在緩落前優先使用閃現 (即使掉落時)"
-				},
-				Monk = {
-					Roll = "武僧: 翻滾",
-					ZenFlight = "武僧: 當移動或掉落時使用御風禪"
-				},
-				Paladin = {
-					DivineSteed = "聖騎士: 神性戰馬"
-				},
-				Priest = {
-					Levitate = "牧師: 漂浮術"
-				},
-				Rogue = {
-					Sprint = "盜賊: 衝刺"
-				},
-				Shaman = {
-					GhostWolf = "薩滿: 鬼魂之狼"
-				},
-				Evoker = {
-					Hover = "唤魔师: 悬空"
-				}
-			},
-			Profiles = {
-				CreateNew = "創建新的設定檔:",
-				Current = "當前設定檔:",
-				CopyFrom = "複製設定檔:",
-				Delete = "刪除設定檔:"
-			}
-		}
-	}
-end
+local L = LibStub("AceLocale-3.0"):NewLocale("BeStride", "zhTW")
+if not L then return end
+
+
+L["GUI.Options.Open.GUI"] = "開啟設定"
+L["GUI.BUTTON.SelectAll"] = "全部選擇"
+L["GUI.BUTTON.ClearAll"] = "清除全選"
+L["GUI.BUTTON.Filter"] = "過濾器"
+L["GUI.BUTTON.Copy"] = "複製"
+L["GUI.BUTTON.Delete"] = "刪除"
+L["GUI.TAB.Mounts"] = "坐騎"
+L["GUI.TAB.MountOptions"] = "坐騎選項"
+L["GUI.TAB.ClassOptions"] = "職業選項"
+L["GUI.TAB.Keybinds"] = "快捷鍵"
+L["GUI.TAB.Profiles"] = "設定檔"
+L["GUI.TAB.About"] = "關於"
+L["GUI.TAB.Mounts.Ground"] = "地面坐騎"
+L["GUI.TAB.Mounts.Flying"] = "飛行座騎"
+L["GUI.TAB.Mounts.Repair"] = "維修坐騎"
+L["GUI.TAB.Mounts.Swimming"] = "游泳坐騎"
+L["GUI.TAB.Mounts.Passenger"] = "多載坐騎"
+L["GUI.TAB.Mounts.Special"] = "特殊坐騎"
+L["GUI.About"] = "版本: " .. version .. "\n" .. "作者: " .. author .. "\n" .. "描述: " .. "\n\n" .. "        " .. "BeStride 是由 YayMounts 改版而來原始作者為 Windrunner US 的 Cyrae 和 Kirin Tor US 的 Anzu" .. "\n" .. "        " .. "接下來, Burning Legion US 的 Anaximander 發現這個專案被原始作者放棄了, 並且有一些錯誤需要被修正" .. "\n" .. "        " .. "在解決錯誤的過程中, 此插件被現代化以讓程式碼更加的精簡與模組化." .. "\n\n" .. "特別感謝:" .. "\n\n" .. "        " .. "Mindlessgalaxy: 幫助 beta 版本的測試"
+L["Bindings.Header"] = "BeStride"
+L["Bindings.Regular"] = "騎乘按鈕"
+L["Bindings.Ground"] = "強制地面坐騎按鈕"
+L["Bindings.Repair"] = "強制維修坐騎按鈕"
+L["Bindings.Passenger"] = "強制多載坐騎按鈕"
+--L["Zone.AzuremystIsle"] = "Azuremyst Isle"
+--L["Zone.BloodmystIsle"] = "Bloodmyst Isle"
+L["Zone.Deadmines"] = "死亡礦坑"
+L["Zone.AQ"] = "安其拉"
+L["Zone.Dalaran"] = "達拉然"
+L["Zone.Dalaran.SubZone.Underbelly"] = "城底區"
+L["Zone.Dalaran.SubZone.UnderbellyDescent"] = "城底陷窟"
+L["Zone.Dalaran.SubZone.CircleofWills"] = "意志之環"
+L["Zone.Dalaran.SubZone.BlackMarket"] = "黑市"
+--L["Zone.Dalaran.SubZone.KrasusLanding"] = ""
+L["Zone.Icecrown"] = "寒冰皇冠"
+L["Zone.Oculus"] = "奧核之眼"
+L["Zone.StormPeaks"] = "風暴群山"
+L["Zone.SholazarBasin"] = "休拉薩盆地"
+L["Zone.Wintergrasp"] = "冬握湖"
+L["Zone.Vashjir"] = "瓦許伊爾"
+L["Zone.Vashjir.SubZone.KelptharForest"] = "凱波薩爾森林"
+L["Zone.Vashjir.SubZone.ShimmeringExpanse"] = "閃光瀚洋"
+L["Zone.Vashjir.SubZone.AbyssalDepths"] = "地獄深淵"
+L["Zone.Vashjir.SubZone.DamplightChamber"] = "霧光之間"
+L["Zone.Vashjir.SubZone.Nespirah"] = "奈斯畢拉"
+L["Zone.Vashjir.SubZone.LGhorek"] = "勒苟雷克"
+L["Zone.VortexPinnacle"] = "漩渦尖塔"
+L["Zone.Nagrand"] = "納葛蘭"
+L["Continent.Draenor"] = "德拉諾"
+--L["Skills.Riding"] = ""
+L["Settings.EnableNew"] = "當學會新坐騎時自動啟用它們"
+L["Settings.EmptyRandom"] = "如果沒有可以使用的已選擇坐騎時隨機使用"
+L["Settings.RemountAfterDismount"] = "下馬後立刻重新上馬"
+L["Settings.NoDismountWhileFlying"] = "在飛行時不解除坐騎. 你必須要降落或者是施放一個法術 (假使在選項中啟用後)"
+L["Settings.UseFlyingMount"] = "在不可飛行的區域仍然使用飛行座騎"
+L["Settings.ForceFlyingMount"] = "在不可飛行的區域強制使用飛行座騎"
+L["Settings.PrioritizePassenger"] = "在隊伍時優先使用多載坐騎"
+L["Settings.NoSwimming"] = "不会使用游泳坐骑"
+L["Settings.FlyingBroom"] = "總是使用魔法掃帚代替普通坐騎"
+L["Settings.Telaari"] = "在納葛蘭時總是使用泰拉蕊塔巴克或是霜狼"
+L["Settings.ForceRobot"] = "當你有學草藥學時總是使用機器人"
+L["Settings.Repair.Use"] = "當到達耐久度閥值時且擁有時使用維修坐騎"
+L["Settings.Repair.Force"] = "當角色有維修坐騎時強制使用"
+L["Settings.Repair.Durability"] = "物品低耐久度 %"
+L["Settings.Repair.GlobalDurability"] = "全局低耐久度 %"
+L["Settings.Repair.InventoryDurability"] = "背包物品低耐久度 %"
+L["Settings.Classes.DeathKnight"] = "死亡騎士"
+L["Settings.Classes.DemonHunter"] = "惡魔獵人"
+L["Settings.Classes.Druid"] = "德魯伊"
+L["Settings.Classes.Evoker"] = "唤魔师"
+--L["Settings.Classes.Hunter"] = ""
+L["Settings.Classes.Mage"] = "法師"
+L["Settings.Classes.Monk"] = "武僧"
+L["Settings.Classes.Paladin"] = "聖騎士"
+L["Settings.Classes.Priest"] = "牧師"
+L["Settings.Classes.Rogue"] = "盜賊"
+L["Settings.Classes.Shaman"] = "薩滿"
+--L["Settings.Classes.Warlock"] = ""
+--L["Settings.Classes.Warrior"] = ""
+L["Settings.Classes.DeathKnight.WraithWalk"] = "死亡騎士: 闇境靈行"
+L["Settings.Classes.DemonHunter.FelRush"] = "惡魔獵人: 使用魔化衝刺"
+L["Settings.Classes.DemonHunter.Glide"] = "惡魔獵人: 使用滑翔"
+L["Settings.Classes.Druid.FlightForm"] = "德魯伊: 使用飛行型態"
+L["Settings.Classes.Druid.TravelToTravel"] = "德魯伊: 在戰鬥時, 從旅行或水棲型態直接切換回那個型態"
+L["Settings.Classes.Druid.FlightFormPriority"] = "德魯伊: 即使未移動時也優先使用飛行型態"
+L["Settings.Classes.Druid.MountedToFlightForm"] = "德魯伊: 當在飛行座騎上且正在飛行時,變換為飛行型態"
+--L["Settings.Classes.Hunter.AspectOfTheCheetah"] = ""
+L["Settings.Classes.Mage.SlowFall"] = "法師: 緩落"
+L["Settings.Classes.Mage.Blink"] = "法師: 閃現"
+L["Settings.Classes.Mage.BlinkPriority"] = "法師: 在緩落前優先使用閃現 (即使掉落時)"
+L["Settings.Classes.Monk.Roll"] = "武僧: 翻滾"
+L["Settings.Classes.Monk.ZenFlight"] = "武僧: 當移動或掉落時使用御風禪"
+L["Settings.Classes.Paladin.DivineSteed"] = "聖騎士: 神性戰馬"
+L["Settings.Classes.Priest.Levitate"] = "牧師: 漂浮術"
+L["Settings.Classes.Rogue.Sprint"] = "盜賊: 衝刺"
+L["Settings.Classes.Shaman.GhostWolf"] = "薩滿: 鬼魂之狼"
+L["Settings.Classes.Evoker.Hover"] = "唤魔师: 悬空"
+L["Settings.Profiles.CreateNew"] = "創建新的設定檔:"
+L["Settings.Profiles.Current"] = "當前設定檔:"
+L["Settings.Profiles.CopyFrom"] = "複製設定檔:"
+L["Settings.Profiles.Delete"] = "刪除設定檔:"
