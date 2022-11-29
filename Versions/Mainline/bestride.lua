f0cc711073d9d7e463ac284e44fdd797b0ca8596
Anaximander <anaximander@ironfistgaming.net>
2022-11-29 00:03:39 -0600

    Add helpers for wrath/mainline to get mount info

--- Versions/Mainline/bestride.lua
+++ Versions/Mainline/bestride.lua
@@ -9,3 +9,33 @@ end
 function BeStride:IsMainline()
     return true
 end
+
+function BeStride:GetMountInfoBySpellID(id)
+    local mountID = C_MountJournal.GetMountFromSpell(id)
+    if mountID then
+        return self:GetMountInfoByMountID(mountID)
+    end
+end
+
+function BeStride:GetMountInfoByMountID(id)
+    local creatureName,spellID,icon,active,isUsable,sourceType,isFavorite,isFactionSpecific,faction,hideOnChar,isCollected,mountID,isForDragonriding = C_MountJournal.GetMountInfoByID(id)
+    return {
+        creatureName = creatureName,
+        spellID = spellID,
+        mountID = mountID,
+        icon = icon,
+        active = active,
+        isUsable = isUsable,
+        sourceType = sourceType,
+        isFavorite = isFavorite,
+        isFactionSpecific = isFactionSpecific,
+        faction = faction,
+        hideOnChar = hideOnChar,
+        isCollected = isCollected,
+        isForDragonriding = isForDragonriding
+    }
+end
+
+function BeStride:GetMountInfoByIndex(index)
+    return nil
+end
